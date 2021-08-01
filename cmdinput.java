c41a50f8aaae115a1098c0ef949d848b535548c5
shivamMahobiya123 <dunzimmaster@gmail.com>
2021-08-01 13:05:40 +0530

    command input

---
+++ cmdinput.java
@@ -0,0 +1,11 @@
+class cmdinput
+{
+	public static void main(String args[])
+	{
+		for(int i=0;args.length;i++)
+		{
+			System.out.println(args[i]+" ");
+		}
+	System.exit(0);
+	}
+}
---
+++ hello.java
@@ -0,0 +1,9 @@
+class hello
+{
+	public static void main(String args[])
+	{
+	int i=0;
+	i=Integer.parseInt(args[0]);
+	System.out.println("Hello World "+i);
+	}
+}
